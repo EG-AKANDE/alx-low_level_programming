1fc0c58fdeb46c1872db7ffdf17ad9631434f3fe
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-03-23 16:45:31 -0700

    function that prints the numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_numbers - Prints the numbers from 0-9
+ * Return: The numbers since 0 up to 9
  */
+
 void print_numbers(void)
+
 {
 	int num;
 
