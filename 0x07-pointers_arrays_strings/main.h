f677295723b9f0f7771a6b5cf789eb0212f2a949
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-03 12:50:57 -0700

    function that copies memory area.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,12 +2,15 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * _memset: this would fill first specified bytes of memory pointed
  *           to with a constant byte
+ * _memcpy: copies n bytes from memory area
  */
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
