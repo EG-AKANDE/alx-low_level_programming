0f319a2872bc52d0e06c7091c3af0cbb1e4fb491
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-03 12:06:02 -0700

    function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x07-pointers_arrays_strings
+0-memset.c: function that fills memory with a constant byte.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _memset: this would fill first specified bytes of memory pointed
+ *           to with a constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
