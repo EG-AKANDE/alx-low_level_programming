408358d60c1d8daeb0db9caa4fb949eda60850f2
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-05 22:19:04 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,35 @@
 #include "main.h"
 
 /**
+ * is_palindrome_recursive - check string for palindrome
+ * @s: string
+ * @start: start string
+ * @end: end of string
+ *
+ * Return: 1 if palindrome, 0 if else
+ */
+
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	if (start >= end)
+	return (1);
+	if (*(s + start) != *(s + end))
+	return (0);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks string for palindrome
  * @s: string
  *
- * Return: 1 if palindrome, 0 if not
+ * Return: 1 if palindrome, 0 if else
  */
 
 int is_palindrome(char *s)
 {
 	int len = 0;
-	char *end;
 
-	while (*s != '\0')
-{
-	s++;
+	while (*(s + len) != '\0')
 	len++;
-}
-	end = s - 1;
-	while (s < end)
-{
-	if (*s != *end)
-{
-	return (0);
-}
-	s++;
-	end--;
-}
-	return (1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
