fa52f5a894ca070d21fba514a0cce3e7bb264077
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-17 17:37:13 -0700

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: dog structure pntr
+ */
+
+void free_dog(dog_t *d)
+
+{
+	if (d == NULL)
+	return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
