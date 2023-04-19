4c873246f595e52dc2e2b66b5e4e2d297086d95f
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-19 14:37:30 -0700

    program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry
+ *
+ * @argc: Argument count
+ * @argv: Arguments verified
+ *
+ * Return: 98 and print error plus newline if wrong argc,
+ * 99 and print error plus newline if operator unfound,
+ * 100 then newline for division or mod by 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+{
+	printf("Error\n");
+	return (98);
+}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+{
+	printf("Error\n");
+	return (100);
+}
+
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+{
+	printf("Error\n");
+	return (99);
+}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns sum a plus b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum a plus b
+ */
+
+int op_add(int a, int b)
+
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns diff a and b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: diff a and b
+ */
+
+int op_sub(int a, int b)
+
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product a by b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product a by b
+ */
+
+int op_mul(int a, int b)
+
+{
+	return (a * b);
+}
+
+/**
+ * op_div - result of div a by b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result of a by b
+ */
+
+int op_div(int a, int b)
+
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of div a / b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: remainder of div a / b
+ */
+
+int op_mod(int a, int b)
+
+{
+	return (a % b);
+}
