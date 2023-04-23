c98fffdcd8e92f6d8f3418460e6d0cf8921ed310
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-22 22:22:22 -0700

    correction of task 3 main file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,28 +20,30 @@ int main(int argc, char *argv[])
 	int (*op_func)(int, int);
 
 	if (argc != 4)
-{
-	printf("Error\n");
-	return (98);
-}
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
-{
-	printf("Error\n");
-	return (100);
-}
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
-{
-	printf("Error\n");
-	return (99);
-}
+
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	result = op_func(num1, num2);
+
 	printf("%d\n", result);
 
 	return (0);
